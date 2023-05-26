ae8f0f102c3f6853b56e2fee4505f64268f15f9d
ycx-8 <120286892+ycx-8@users.noreply.github.com>
2023-05-26 10:22:27 +1000

    cris fix: str msg decalred as vars

--- Calculator_1_Method.java
+++ Calculator_1_Method.java
@@ -12,6 +12,20 @@ public class Calculator_1_Method {
         System.out.println("[+] Welcome to this calcultor built only with var, conditionals, loops and arrays!");
         System.out.println("[+] You can type in the digits 0000 at any time to quit the program");
 
+        //Declare all messages as var
+        String enter1stNum = "[+] Enter a number: ";
+        String listAllOp = "[+] Here are all the operations you can perform:";
+        String opList = "   +   -   *   /   ";
+        String enterOp = "[+] Enter the symbol of operation that you want to use: ";
+        String enterMoreNum = "[+] Enter another number: ";
+        String cannotDivide0 = "[-] A number cannot be divided by 0 - enter another number: ";
+        String continueOp = "[?] Continue the operations y/n? ";
+        String writeToTxt = "[?] Append the result to a txt file? y/n";
+        String invalidInput1 = "[-] Invalid input: type 'y' to continue the operation or 'n' to stop and print out the results:";
+        String thankYou = "Thank you for using Chenxi's calculator!";
+        String finalOptions = "[?] Enter 1 to perform a new calculation || Enter 2 to read last result || Enter 3 to see result history || Enter 4 to quit ";
+        String invalidInput2 = "[-] Invalid input, try again.";
+
         // Storing history of the results 
         double[] history = new double[50];
         int x = 0;
@@ -20,7 +34,7 @@ public class Calculator_1_Method {
         while(true) {
 
             // Input 1st number
-            System.out.println("[+] Enter a number: ");
+            System.out.println(enter1stNum);
             int num1 = input.nextInt();
             input.nextLine();
             if (num1 == 0000) System.exit(0);
@@ -32,22 +46,22 @@ public class Calculator_1_Method {
             while (true) {
 
                 // Input operation
-                System.out.println("[+] Here are all the operations you can perform:");
+                System.out.println(listAllOp);
                 // System.out.println();
-                System.out.println("   +   -   *   /   ");
+                System.out.println(opList);
                 // System.out.println();
-                System.out.println("[+] Enter the symbol of operation that you want to use: ");
+                System.out.println(enterOp);
                 String op = input.nextLine();
                 if (op.equals("0000")) System.exit(0);
 
                 // Input another number
-                System.out.println("[+] Enter another number: ");
+                System.out.println(enterMoreNum);
                 int numX;
                 while (true) {
                     numX = input.nextInt();
                     if (numX == 0000) System.exit(0);
                     if (numX == 0 && op.equals("/")) {
-                        System.out.println("[-] A number cannot be divided by 0 - enter another number: ");
+                        System.out.println(cannotDivide0);
                     } else break;
                 } 
 
@@ -66,7 +80,7 @@ public class Calculator_1_Method {
                 }
 
                 // Stop calculation or continue adding operation and numbers
-                System.out.println("[?] Continue the operations y/n? ");
+                System.out.println(continueOp);
                 while (true) {
                     String continueOrNo = input.nextLine();
                    
@@ -80,14 +94,14 @@ public class Calculator_1_Method {
                         x++;
 
                         // Write to file
-                        System.out.println("Append the result to a txt file? y/n");
+                        System.out.println(writeToTxt);
                         while (true) {
                             String writeOrNo = input.nextLine();
                             try {
                                 if (writeOrNo.equals("y")) {
                                     BufferedWriter writeResult = new BufferedWriter(new FileWriter("C:\\Users\\ycx-8\\Desktop\\Result.txt"));
                                     writeResult.write("Result = " + result + "\n\n\n\n");
-                                    writeResult.write("Thank you for using Chenxi's calculator!");
+                                    writeResult.write(thankYou);
                                     writeResult.close();
                                     break;
                                 } else if (writeOrNo.equals("n")) break;
@@ -99,7 +113,7 @@ public class Calculator_1_Method {
                         exit1 = true;
                         break;
                     } else {
-                        System.out.println("[-] Invalid input: type 'y' to continue the operation or 'n' to stop and print out the results:");
+                        System.out.println(invalidInput1);
                     }
                 } 
 
@@ -109,7 +123,7 @@ public class Calculator_1_Method {
             
 
             // Restart or quit + file i/o
-            System.out.println("[?] Enter 1 to perform a new calculation || Enter 2 to read last result || Enter 3 to see result history || Enter 4 to quit ");
+            System.out.println(finalOptions);
             while (true) {
                 int redoOrQuit = input.nextInt();
                 input.nextLine();
@@ -124,7 +138,7 @@ public class Calculator_1_Method {
                             System.out.println(fileResult);
                         }
                         readResult.close();
-                        System.out.println("[?] Enter 1 to perform a new calculation || Enter 2 to read last result || Enter 3 to see result history || Enter 4 to quit ");
+                        System.out.println(finalOptions);
                         continue;
                         } catch (Exception e) {
                         return;
@@ -136,12 +150,12 @@ public class Calculator_1_Method {
                             System.out.println("Result = " + history[i]);
                         }
                     }
-                    break;
+                    continue;
                 } else if (redoOrQuit == 4) {
                     input.close();
                     System.exit(0);
                 } else {
-                    System.out.println("[-] Invalid input, try again.");
+                    System.out.println(invalidInput2);
                 }
     
             }
